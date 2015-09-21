e3db28a61de4933b145ff1faf2fcd2a443224dc5
tienduyle <tienl@hostworks.com.au>
2015-09-21 16:44:51 +0930

    Initial Commit

---
+++ app/src/main/java/com/mediafoundry/lib/example/Home.java
@@ -0,0 +1,10 @@
+package com.mediafoundry.lib.example;
+
+import com.mediafoundry.lib.core.base.BaseActivity;
+
+/**
+ * Created by tienl on 21/09/2015.
+ */
+public class Home extends BaseActivity {
+
+}
